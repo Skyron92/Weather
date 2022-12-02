8d64572539a7d6bed1ad261abdfed028ae211454
Skyron92 <courtier.leo@gmail.com>
2022-12-02 16:38:00 +0000

    Camera

--- Assets/Script/Camera.cs
+++ Assets/Script/Camera.cs
@@ -1,12 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    private float _y;
+    private float _x;
+
+    private void Update()
+    {
+        MouseMove();
+    }
+
     void MouseMove()
     {
-        
+        _x += Input.mousePosition.x;
+        transform.rotation = new Quaternion(0, _x, 0, 0);
     }
 }
